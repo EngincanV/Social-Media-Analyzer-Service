3ea9d34df63c5cad404dbe8ba0212bda60667825
EngincanV <enginveske@gmail.com>
2020-12-30 23:02:22 +0300

    Create SubscriptionService and move related operations into it

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -4,8 +4,8 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 
-//enums
-const SubscriptionTypes = require("../constants/SubscriptionType");
+//services
+const { addUserToSubscription } = require("../services/SubscriptionService");
 
 //photo storage
 const storage = multer.diskStorage({
@@ -176,57 +176,6 @@ router.post("/add-profile-photo", upload.single("profilePhoto"), (req: any, res:
         }
     }) 
 });
-
-function addUserToSubscription(userId: number) {
-    const db = mysql.createConnection(dbConfig);
-    var date = new Date();
-    var subscriptionStartDate = date.toISOString().split("T")[0];
-
-    date.setMonth(date.getMonth() + 6);
-    var subscriptionEndDate: string = date.toISOString().split("T")[0];
-    var subscriptionPrice: number = getSubscriptionPriceById(SubscriptionTypes.Free);
-    var subscriptionMonth = 6; //TODO: get this from user, after MVP?
-    var totalAmount = subscriptionPrice * subscriptionMonth;
-
-    let sqlCommand: string = `INSERT INTO subscriptions (userId, subscriptionTypeId, subscriptionStartDate, subscriptionEndDate, subscriptionMonthCount, totalAmount) VALUES (${userId}, ${SubscriptionTypes.Free}, "${subscriptionStartDate}", "${subscriptionEndDate}", ${subscriptionMonth}, ${totalAmount})`;
-
-    db.connect((err: any) => {
-        if (err) {
-            console.log('Cannot connect database');
-            throw err;
-        }
-
-        db.query(sqlCommand, (err: any, results: any) => {
-            if (err)
-                throw err;
-        });
-    });
-}
-
-function getSubscriptionPriceById(subcriptionId: number): number {
-    const db = mysql.createConnection(dbConfig);
-
-    let sqlCommand: string = `SELECT price FROM subscriptionTypes WHERE id = ${subcriptionId}`;
-    console.log(sqlCommand)
-
-    db.connect((err: any) => {
-        if(err) {
-            console.log("Cannot connect db");
-            throw err;
-        }
-
-        db.query(sqlCommand, (err: any, results: any) => {
-            if (err)
-                throw err;
-    
-            const price = results[0].price;
-
-            return price;
-        });
-    });
-
-    return 0;
-}
 
 function isUsernameExist(username: string): any {
     const db = mysql.createConnection(dbConfig);
